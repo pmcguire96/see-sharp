5968ffc8565c2ed05b7782c12e24ecbd68bfd53e
Patrick <pmcguire96@gmail.com>
2026-03-25 21:52:07 -0500

    scratch

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # see-sharp
-see... sharp?
+
---
+++ asdf.sql
@@ -0,0 +1,9 @@
+CREATE TABLE clients (
+  primarykey INT,
+  client_id INT,
+  num INT,
+  replacement VARCHAR(255),
+);
+
+
+-- int number : string replacement
